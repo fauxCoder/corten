fd9c6c9cee257b71727492b352e2291146b3969c
Jameson York <jamesonyork8@gmail.com>
2017-04-05 20:39:16 +1000

    Tutes.

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,40 @@
 extern crate sdl2;
 
+mod events;
+
 use sdl2::pixels::Color;
-use sdl2::event::Event;
-use sdl2::keyboard::Keycode;
+use events::Events;
 
-pub fn main() {
+fn main() {
+    // Initialize SDL2
     let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
+    let video = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("rust-sdl2 demo: Video", 800, 600)
-        .position_centered()
-        .opengl()
-        .build()
-        .unwrap();
+    // Create the window
+    let window = video.window("ArcadeRS Shooter", 800, 600)
+        .position_centered().opengl()
+        .build().unwrap();
 
-    let mut renderer = window.renderer().build().unwrap();
+    let mut renderer = window.renderer()
+        .accelerated()
+        .build().unwrap();
 
-    renderer.set_draw_color(Color::RGB(255, 0, 0));
+    // Render a fully black window
+    renderer.set_draw_color(Color::RGB(0, 0, 0));
     renderer.clear();
     renderer.present();
-    let mut event_pump = sdl_context.event_pump().unwrap();
 
-    'running: loop {
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit {..} | Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
-                },
-                _ => {}
-            }
+    let mut events = Events::new(sdl_context.event_pump().unwrap());
+
+    loop {
+        events.pump();
+
+        if events.quit || events.key_escape {
+            break;
         }
-        // The rest of the game loop goes here...
+
+        renderer.set_draw_color(Color::RGB(0, 0, 0));
+        renderer.clear();
+        renderer.present();
     }
 }
