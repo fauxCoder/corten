0b05ae08f97e48dbd9476d0f68c34b3b745eebf9
Jameson York <jamesonyork8@gmail.com>
2017-04-17 21:33:24 +1000

    Emscripten support. Ported lots of stuff from squared. Compiles.

--- src/events.rs
+++ src/events.rs
@@ -1,44 +1,91 @@
-use sdl2::EventPump;
+macro_rules! struct_events {
+    (
+        keyboard: { $( $k_alias:ident : $k_sdl:ident ),* },
+        else: { $( $e_alias:ident : $e_sdl:pat ),* }
+    )
+    => {
+        use sdl2::EventPump;
 
-pub struct Events {
-    pump: EventPump,
-
-    pub quit: bool,
-    pub key_escape: bool,
-}
-
-impl Events {
-    pub fn new(pump: EventPump) -> Events {
-        Events {
-            pump: pump,
-
-            quit: false,
-            key_escape: false,
+        pub struct ImmediateEvents {
+            $( pub $k_alias : Option<bool> , )*
+            $( pub $e_alias : bool ),*
         }
-    }
 
-    /// Update the events record.
-    pub fn pump(&mut self) {
-        // If the SDL context is dropped, then poll_iter() will simply stop
-        // yielding any input.
-        for event in self.pump.poll_iter() {
-            use sdl2::event::Event::*;
-            use sdl2::keyboard::Keycode::*;
+        impl ImmediateEvents {
+            pub fn new() -> ImmediateEvents {
+                ImmediateEvents {
+                    $( $k_alias: None , )*
+                    $( $e_alias: false ),*
+                }
+            }
+        }
 
-            match event {
-                Quit { .. } => self.quit = true,
+        pub struct Events {
+            pump: EventPump,
+            pub now: ImmediateEvents,
 
-                KeyDown { keycode, .. } => match keycode {
-                    Some(Escape) => self.key_escape = true,
-                    _ => {}
-                },
+            // true  => pressed
+            // false => not pressed
+            $( pub $k_alias: bool ),*
+        }
 
-                KeyUp { keycode, .. } => match keycode {
-                    Some(Escape) => self.key_escape = false,
-                    _ => {}
-                },
+        impl Events {
+            pub fn new(pump: EventPump) -> Events {
+                Events {
+                    pump: pump,
+                    now: ImmediateEvents::new(),
 
-                _ => {}
+                    // By default, initialize every key with _not pressed_
+                    $( $k_alias: false ),*
+                }
+            }
+
+            pub fn pump(&mut self) {
+                self.now = ImmediateEvents::new();
+
+                for event in self.pump.poll_iter() {
+                    use sdl2::event::Event::*;
+                    use sdl2::keyboard::Keycode::*;
+
+                    match event {
+                        KeyDown { keycode, .. } => match keycode {
+                            // $( ... ),* containing $k_sdl and $k_alias means:
+                            //   "for every element ($k_alias : $k_sdl) pair,
+                            //    check whether the keycode is Some($k_sdl). If
+                            //    it is, then set the $k_alias fields to true."
+                            $(
+                                Some($k_sdl) => {
+                                    // Prevent multiple presses when keeping a key down
+                                    // Was previously not pressed?
+                                    if !self.$k_alias {
+                                        // Key pressed
+                                        self.now.$k_alias = Some(true);
+                                    }
+
+                                    self.$k_alias = true;
+                                }
+                            ),* // and add a comma after every option
+                            _ => {}
+                        },
+
+                        KeyUp { keycode, .. } => match keycode {
+                            $(
+                                Some($k_sdl) => {
+                                    // Key released
+                                    self.now.$k_alias = Some(false);
+                                    self.$k_alias = false;
+                                }
+                            ),*
+                            _ => {}
+                        },
+                        $(
+                            $e_sdl => {
+                                self.now.$e_alias = true;
+                            }
+                        )*,
+                        _ => {}
+                    }
+                }
             }
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,102 @@
 extern crate sdl2;
 
+use sdl2::pixels::Color;
+use sdl2::pixels::PixelFormatEnum;
+use sdl2::rect::{Rect};
+use std::process;
+use std::vec::Vec;
+
+#[cfg(target_os = "emscripten")]
+pub mod emscripten;
+
+#[macro_use]
 mod events;
 
-use sdl2::pixels::Color;
-use events::Events;
+mod temp;
+
+struct_events! {
+    keyboard: {
+        key_left: Left,
+        key_right: Right,
+        key_up: Up,
+        key_down: Down,
+        key_escape: Escape
+    },
+    else: {
+        quit: Quit { .. }
+    }
+}
+
+fn tubby(x: u32, y: u32) -> Option<Vec<usize>>
+{
+    None
+}
+
+fn blendy(background: Color, top: Color) -> Color
+{
+    Color::RGBA(0, 255, 0, 255)
+}
 
 fn main() {
-    // Initialize SDL2
     let sdl_context = sdl2::init().unwrap();
     let video = sdl_context.video().unwrap();
 
     // Create the window
-    let window = video.window("ArcadeRS Shooter", 800, 600)
+    let window = video.window("Corten", 800, 600)
         .position_centered().opengl()
         .build().unwrap();
 
     let mut renderer = window.renderer()
         .accelerated()
+        .present_vsync()
         .build().unwrap();
 
-    // Render a fully black window
-    renderer.set_draw_color(Color::RGB(0, 0, 0));
-    renderer.clear();
-    renderer.present();
+    let img_func = temp::ImageFunction::new(
+        vec![Color::RGBA(0, 0, 0, 0)],
+        tubby,
+        blendy,
+    );
+
+    let mut texture = renderer.create_texture_streaming(PixelFormatEnum::RGBA8888, 256, 256).unwrap();
+    // texture.update(None, , 4).unwrap();
 
     let mut events = Events::new(sdl_context.event_pump().unwrap());
 
-    loop {
+    let mut main_loop = || {
         events.pump();
 
-        if events.quit || events.key_escape {
-            break;
+        if events.now.quit || events.now.key_escape == Some(true) {
+            process::exit(1);
         }
 
-        renderer.set_draw_color(Color::RGB(0, 0, 0));
+        if events.key_left {
+            // rect.x -= 4;
+        }
+
+        if events.key_right {
+            // rect.x += 4;
+        }
+
+        if events.key_up {
+            // rect.y -= 4;
+        }
+
+        if events.key_down {
+            // rect.y += 4;
+        }
+
+        renderer.set_draw_color(Color::RGB(54, 54, 54));
         renderer.clear();
+        renderer.copy(&texture, None, Some(Rect::new(100, 100, 256, 256))).unwrap();
         renderer.present();
-    }
+    };
+
+    #[cfg(target_os = "emscripten")]
+    use emscripten::{emscripten};
+
+    #[cfg(target_os = "emscripten")]
+    emscripten::set_main_loop_callback(main_loop);
+
+    #[cfg(not(target_os = "emscripten"))]
+    loop { main_loop(); }
 }
